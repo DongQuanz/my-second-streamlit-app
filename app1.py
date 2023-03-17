d9d17a57aad0082a7f01eec01487e68fcdca5575
DongQuanz <99068907+DongQuanz@users.noreply.github.com>
2023-03-17 21:15:43 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pickle
+import numpy as np
+import sklearn
 
 pickled_model = pickle.load(open('model.pickle', 'rb')
-st.title('Revenue Prediction')
 
-st.text('Input Temperature')
-a = float(st.number_input('Text here!!!'))
+st.title('Revenue Prediction')
+a = np.array(float(st.number_input('Input Temperature'))).reshape(-1,1)
 if st.button('Predict'):
     st.text('Revenue Prediction')
     y_pred = pickle_model.predict(a)
-    st.success(text)
+    st.success(*y_pred)
