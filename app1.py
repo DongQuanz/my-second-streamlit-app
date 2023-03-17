35d42ff9ce0f12a8ec82f1d6f28518a24492f6ba
DongQuanz <99068907+DongQuanz@users.noreply.github.com>
2023-03-17 20:56:20 +0700

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pickle
+
+pickled_model = pickle.load(open('model.pickle', 'rb')
+st.title('Revenue Prediction')
+
+st.text('Input Temperature')
+a = float(st.number_input('Text here!!!'))
+if st.button('Predict'):
+    st.text('Revenue Prediction')
+    y_pred = pickle_model.predict(x)
+    st.success(text)
